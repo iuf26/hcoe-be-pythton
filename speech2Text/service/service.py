ba9b1533fd880824068f2d90fd63825495ad70d1
Iulia26 <ifilimon26@gmail.com>
2022-07-23 19:32:53 +0300

    added filename paramter

--- speech2Text/service/service.py
+++ speech2Text/service/service.py
@@ -3,7 +3,7 @@ from speech2Text.utils.api_02 import upload,save_transcript
 
 #functia returneaza json-ul returnat de api  deep speech
 # daca totul a decurs ok si in rest returneaza un mesaj de eroare
-def convert_to_text_Deep_Speech():
+def convert_to_text_Deep_Speech(filename):
     #aici completeaza tu
     pass
 
@@ -15,7 +15,7 @@ def convert_to_text(file,textResultFilename,api):
     if api == 0:
         data,error = save_transcript(audio_url, './speech2Text/savings/'+textResultFilename)
     elif api == 1:
-        #data, error = convert_to_text_Deep_Speech()
+        #data, error = convert_to_text_Deep_Speech(filename)
         data,error = "data",""
 
     return data, error
